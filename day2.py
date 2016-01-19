a880ac1ff4ea70fb6459a22f9988b6129ce267bd
Joël Costamagna <joel.costamagna@etu.umontpellier.fr>
2016-01-19 16:50:16 +0100

    finish

--- day2.py
+++ day2.py
@@ -4,4 +4,11 @@
 # h profondeur
 #quantité de papier par cadeau = 2*l*w + 2*w*h + 2*h*l + w*h
 # cadeau sous la forme lxwxh
-# on additionne tous les l , tous les w, tous les h, et on applqiue la formule :
+total = 0
+for line in open('day2.txt'):
+    l, w, h = line.split('x')
+    l, w, h = int(l), int(w), int(h)
+    aire = 2*l*w + 2*w*h + 2*h*l
+    slack = min(l*w, w*h, h*l)
+    total += area + slack
+print total
