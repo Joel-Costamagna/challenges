db7eedd7d9e09d7d90a8d2691fe611a77c959b54
Joël Costamagna <joel.costamagna@etu.umontpellier.fr>
2016-01-19 18:13:08 +0100

    j'aime les licornes
    j'aime les licornes

--- day1.sh
+++ day1.sh
@@ -12,6 +12,25 @@ for (( i = 0; i < ${#input}; i++ )); do
   if [[ $character == ')' ]]; then
     floor=$(($floor-1))
   fi
-echo $floor;
+  echo $floor;
 done
-exit 0
+
+###part two :
+echo "\n\n\n"
+for (( i = 0; i < ${#input}; i++ )); do
+  character=${input:$i:1} #on prend le caractére à la position i
+  if [[ $character == '(' ]]; then
+    floor=$(($floor + 1))
+    if [[$floor == -1]]; then
+        echo i
+        exit 0
+    fi
+  fi
+  if [[ $character == ')' ]]; then
+    floor=$(($floor-1))
+    if [[$floor == -1]]; then
+        echo i
+        exit 0
+    fi
+  fi
+ done
