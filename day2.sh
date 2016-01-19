e2f88f504ae9350c6b5bdf1df26a3354f8fbcef5
Joël <joel.costamagna@etu.umontpellier.fr>
2016-01-19 12:52:53 +0100

    hypothèse de départ et prerequis

--- day2.sh
+++ day2.sh
@@ -3,3 +3,5 @@
 # w largeur
 # h profondeur
 #quantité de papier par cadeau = 2*l*w + 2*w*h + 2*h*l + w*h
+# cadeau sous la forme lxwxh
+# on additionne tous les l , tous les w, tous les h, et on applqiue la formule :
