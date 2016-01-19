2352fa70f962fa113d676ae0367020c70ac51f63
Joël <joel.costamagna@etu.umontpellier.fr>
2016-01-19 11:45:06 +0100

    amélioration test et conditions

--- day1.sh
+++ day1.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 input=$(cat entree.txt)
 floor=0
+echo $floor
 for (( i = 0; i < ${#input}; i++ )); do
   character=${input:$i:1} #on prend le caractére à la position i
-  if [[ character -e '(' ]]; then
-    $floor=$(let "$floor+1")
+  if [[ $character == '(' ]]; then
+    floor=$(($floor + 1))
   fi
+  if [[ $character == ')' ]]; then
+    floor=$(($floor-1))
+  fi
+echo $floor;
 done
+exit 0
